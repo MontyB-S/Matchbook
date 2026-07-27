9798ad1a5a1af43fcef61895bd4acb17983d0ee8
Monty Behar-Sheehan <monty081@outlook.com>
2026-07-27 20:08:39 +0100

    inital project setup

---
+++ src/test/java/com/monty/matchbook/MatchbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monty.matchbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
